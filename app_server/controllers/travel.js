01b18ed2aff2759a2ef9bad3dbd122a8ed0d4189
Justin Chevalier <tonysnow86@hotmail.com>
2023-10-30 21:26:35 -0500

    modulethree committed

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 const travel = (req, res) => {
     pageTitle = process.env.npm_package_description + '- Travel';
-    res.render('travel', {title: pageTitle});
+    res.render('travel', {title: pageTitle, trips});
     };
     module.exports = {
         travel
